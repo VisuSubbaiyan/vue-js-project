4e7b1c254059cb2bddac68a1ea9e432a46e35265
Visu Subbaiyan <visusubbaiyan@smavas-MacBook-Pro.local>
2019-07-23 02:02:22 +0200

    first commit

---
+++ src/components/Checkboxes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :id="id" class="checkboxes">
+    <h3 class="filter-label">{{ name }}</h3>
+    <ul class="checkbox-list">
+      <li v-for="(checkboxItem, index) in checkboxes" :key="checkboxItem.name">
+        <label>
+          <input type="checkbox" v-model="checkboxItem.checked" :value="index"/>
+          {{checkboxItem.name}}
+        </label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    checkboxes: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.checkboxes {
+  text-align: left;
+  margin: 10px 20px 0 0;
+
+  h3.filter-label {
+    margin: 5px 0;
+  }
+
+  ul.checkbox-list {
+    list-style: none;
+    display: flex;
+    padding: 0;
+    margin: 0;
+
+    li {
+      margin-right: 15px;
+    }
+  }
+}
+</style>
